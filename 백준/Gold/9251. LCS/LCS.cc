39c54d701da4991a4dfdd7c1b00d31e040757d17
GeunH <114638557+GeunH@users.noreply.github.com>
2023-03-25 18:10:37 +0900

    [Gold V] Title: LCS, Time: 4 ms, Memory: 5940 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int LCS[1001][1001];
+
+int main()
+{
+	string A, B;
+	cin >> A >> B;
+
+	for (int i = 0; i < A.size(); i++)
+	{
+		for (int j = 0; j < B.size(); j++)
+		{
+			if (A[i] == B[j] )
+			{
+				LCS[i+1][j+1] = LCS[i][j] + 1;
+			}
+			else
+			{
+				LCS[i + 1][j + 1] = max(LCS[i][j+1], LCS[i + 1][j]);
+			}
+		}
+	}
+
+	cout << LCS[A.size()][B.size()];
+	return 0;
+}
