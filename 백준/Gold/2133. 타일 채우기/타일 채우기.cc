6163027ededf75361e3e4ec2cbb8b5b5b1fa03da
GeunH <114638557+GeunH@users.noreply.github.com>
2023-05-18 12:41:27 +0900

    [Gold IV] Title: 타일 채우기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/2133. 타일 채우기/타일 채우기.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int dp[31];
+
+int main()
+{
+	int N;
+	cin >> N;
+
+	dp[0] = 1;
+	dp[2] = 3;
+
+	for (int i = 4; i <= N; i = i + 2)
+	{
+		dp[i] = dp[i - 2] * 3;
+		for (int j = 4; j <= i; j = j + 2)
+		{
+			dp[i] += dp[i - j] * 2;
+		}
+	}
+	cout << dp[N];
+	return 0;
+}
