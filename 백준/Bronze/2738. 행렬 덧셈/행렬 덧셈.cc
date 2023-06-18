b532e5d3c5ce347920e337ce159b223951a50dcd
GeunH <114638557+GeunH@users.noreply.github.com>
2023-06-18 16:00:01 +0900

    [Bronze V] Title: 행렬 덧셈, Time: 8 ms, Memory: 2100 KB -BaekjoonHub

---
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.cc
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+int A[100][100], B[100][100];
+
+int main()
+{
+	int N, M;
+
+	cin >> N >> M;
+
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < M; j++)
+		{
+			cin >> A[i][j];
+		}
+	}
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < M; j++)
+		{
+			cin >> B[i][j];
+		}
+	}
+	
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < M; j++)
+		{
+			cout << A[i][j] + B[i][j] << " ";
+		}
+		cout << endl;
+	}
+	return 0;
+}
