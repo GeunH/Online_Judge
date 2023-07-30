3fd04b03e1a9d7cce27d20a9bc4c51323ddff225
GeunH <114638557+GeunH@users.noreply.github.com>
2023-07-30 19:26:02 +0900

    [Silver V] Title: 막대기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1094. 막대기/막대기.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int X, cnt = 0;
+    cin >> X;
+
+    while (X > 0) {
+        if (X % 2 == 1) {
+            cnt++;
+        }
+        X /= 2;
+    }
+
+    cout << cnt;
+
+    return 0;
+}
