25bce15999248f49c956f322244e77659eebcd81
최근혁 <unlxrmsgur@naver.com>
2023-12-19 17:16:51 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.34 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -2,37 +2,10 @@ class Solution {
     public int solution(String s) {
         int answer = 0;
         
-        for( int i = 0 ; i< s.length() ; i++){
-            if( s.charAt(i) == 'z'){
-                s = s.substring(0,i) + '0' + s.substring(i+4);
-            }
-            else if ( s.charAt(i) == 'o'){
-                 s = s.substring(0,i) + '1' + s.substring(i+3);
-            }
-            else if ( s.charAt(i) == 't' && s.charAt(i+1) =='w'){
-                 s = s.substring(0,i) + '2' + s.substring(i+3);
-            }
-            else if ( s.charAt(i) == 't' && s.charAt(i+1) =='h'){
-                 s = s.substring(0,i) + '3' + s.substring(i+5);
-            }
-            else if ( s.charAt(i) == 'f' && s.charAt(i+1) =='o'){
-                 s = s.substring(0,i) + '4' + s.substring(i+4);
-            }
-            else if ( s.charAt(i) == 'f' && s.charAt(i+1) =='i'){
-                 s = s.substring(0,i) + '5' + s.substring(i+4);
-            }
-            else if ( s.charAt(i) == 's' && s.charAt(i+1) =='i'){
-                 s = s.substring(0,i) + '6' + s.substring(i+3);
-            }
-            else if ( s.charAt(i) == 's' && s.charAt(i+1) =='e'){
-                 s = s.substring(0,i) + '7' + s.substring(i+5);
-            }
-            else if ( s.charAt(i) == 'e'){
-                 s = s.substring(0,i) + '8' + s.substring(i+5);
-            }
-             else if ( s.charAt(i) == 'n'){
-                 s = s.substring(0,i) + '9' + s.substring(i+4);
-            }
+        String [] arr = {"zero", "one", "two", "three", "four", "five" , "six", "seven" , "eight" ,"nine"};
+        
+        for ( int i=0; i< arr.length; i++){
+            s= s.replaceAll(arr[i], String.valueOf(i));
         }
         return Integer.parseInt(s);
     }
