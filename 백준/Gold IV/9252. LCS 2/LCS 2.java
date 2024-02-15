26364c35fe845c11eae7eb036cbcd9af08bc9f64
최근혁 <unlxrmsgur@naver.com>
2024-02-15 11:04:58 +0900

    [Gold IV] Title: LCS 2, Time: 180 ms, Memory: 18684 KB -BaekjoonHub

--- 백준/Gold IV/9252. LCS 2/LCS 2.java
+++ 백준/Gold IV/9252. LCS 2/LCS 2.java
@@ -2,59 +2,46 @@ import java.io.*;
 import java.util.*;
 
 public class Main {        
-	static String first;
-	static String second;
+    static String first;
+    static String second;
     
-	static Text[][] texts;
-	
-	static class Text{
-		int cnt;
-		String text;
-		Text(int cnt, String text){
-			this.cnt= cnt;
-			this.text =text;
-		}
-	}
-	
-	public static void main(String args[]) throws Exception
-    {
+    static int[][] cnt;
+    
+    public static void main(String args[]) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         
-        first = st.nextToken();
-        st = new StringTokenizer(br.readLine(), " ");
-        second= st.nextToken();
+        first = br.readLine();
+        second = br.readLine();
         
-        texts = new Text[first.length()+1][second.length()+1];
-       
-        for(int i=0; i<= first.length() ; i++) {
-        	texts[i][0] = new Text(0,"");
-        }
-        for(int j=0; j<= second.length(); j++) {
-        	texts[0][j] = new Text(0,"");
-        }
+        cnt = new int[first.length()+1][second.length()+1];
         
         for(int i=1; i <= first.length(); i++) {
-        	for(int j=1; j<= second.length() ;j++) {
-        		if(first.charAt(i-1) == second.charAt(j-1)) {
-        			if( texts[i-1][j-1].cnt + 1 > texts[i][j-1].cnt) {
-        				texts[i][j] = new Text(texts[i-1][j-1].cnt+1, texts[i-1][j-1].text+ first.charAt(i-1) );
-        			}
-        			else {
-        				texts[i][j] =  texts[i][j-1];
-        			}
-        		}
-        		else {
-        			if( texts[i-1][j].cnt > texts[i][j-1].cnt) {
-        				texts[i][j] = texts[i-1][j];
-        			}
-        			else {
-        				texts[i][j] = texts[i][j-1];
-        			}
-        		}
-        	}
+            for(int j=1; j<= second.length() ;j++) {
+                if(first.charAt(i-1) == second.charAt(j-1)) {
+                    cnt[i][j] = cnt[i-1][j-1]+1;
+                } else {
+                    cnt[i][j] = Math.max(cnt[i-1][j], cnt[i][j-1]);
+                }
+            }
         }
-        System.out.println(texts[first.length()][second.length()].cnt+"\n"+texts[first.length()][second.length()].text);
         
+        System.out.println(cnt[first.length()][second.length()]);
+        
+        StringBuilder lcs = new StringBuilder();
+        int i = first.length(), j = second.length();
+        while(i > 0 && j > 0) {
+            if(cnt[i][j] == cnt[i-1][j]) {
+                i--;
+            } else if(cnt[i][j] == cnt[i][j-1]) {
+                j--;
+            } else {
+                lcs.append(first.charAt(i-1));
+                i--;
+                j--;
+            }
+        }
+        if(cnt[first.length()][second.length()] != 0) {
+            System.out.println(lcs.reverse().toString());
+        }
     }
 }
