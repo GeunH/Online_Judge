b999bd8b660b57b98bb60001afc17359e4b64c5c
최근혁 <unlxrmsgur@naver.com>
2024-02-28 10:54:51 +0900

    [Gold IV] Title: 월드컵, Time: 120 ms, Memory: 14112 KB -BaekjoonHub

--- 백준/Gold/6987. 월드컵/월드컵.java
+++ 백준/Gold/6987. 월드컵/월드컵.java
@@ -1,76 +1,78 @@
-import java.io.*;
 import java.util.*;
 import java.io.*;
-import java.util.*;
-public class Main{
-    static int[] g1 = {0,0,0,0,0,1,1,1,1,2,2,2,3,3,4};
-    static int[] g2 = {1,2,3,4,5,2,3,4,5,3,4,5,4,5,5};
-    static int sumWin;
-    static int sumDraw;
-    static int sumLose;
-    static int[] win ;
-    static int[] draw;
-    static int[] lose;
-    static boolean answer;
 
-    public static void main(String[] args) throws Exception{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-
-        for (int t = 1; t <= 4; t++) {
-            sumWin = sumDraw = sumLose = 0;
-            win = new int[6];
-            draw = new int[6];
-            lose = new int[6];
-            answer = false;
-            st = new StringTokenizer(br.readLine());
-            for (int i = 0; i < 6; i++) {
-                sumWin += win[i] = Integer.parseInt(st.nextToken());
-                sumDraw += draw[i] = Integer.parseInt(st.nextToken());
-                sumLose += lose[i] = Integer.parseInt(st.nextToken());
-            }
-            if(sumWin+sumDraw+sumLose!=30) {
-                answer = false;
-            }else {
-                game(0);
-            }
-            System.out.print((answer==true ? 1:0)+" ");
-            
-        }System.out.println();            
-    }
-    private static void game(int cnt) {
-        if(cnt == 15) {
-            answer = true;
+public class Main {
+    
+    static int[] w, b, l, t;
+    static boolean isCould;
+    
+    static int t1[] = new int[] { 1,1,1,1,1,2,2,2,2,3,3,3,4,4,5};
+    static int t2[] = new int[] { 2,3,4,5,6,3,4,5,6,4,5,6,5,6,6};
+    
+    static void startGame(int gameIndex) {
+        if (gameIndex == 15) {
+            isCould = true; 
             return;
         }
-        int t1 = g1[cnt];
-        int t2 = g2[cnt];
-        //t1이 이기는 게 가능한 경우
-        if(win[t1]>0 && lose[t2]>0) {
-            win[t1]--;
-            lose[t2]--;
-            game(cnt+1);
-            win[t1]++;
-            lose[t2]++;    
-        }
-        //t2가 이기는 게 가능한 경우
-        if(lose[t1]>0 && win[t2]>0) {
-            lose[t1]--;
-            win[t2]--;
-            game(cnt+1);
-            lose[t1]++;
-            win[t2]++;
-        }
-        //무승부가 가능한 경우
-        if(draw[t1]>0 && draw[t2]>0) {
-            draw[t1]--;
-            draw[t2]--;
-            game(cnt+1);
-            draw[t1]++;
-            draw[t2]++;        
+        if (isCould) return;
+        {
+        	int teamA = t1[gameIndex];
+        	int teamB = t2[gameIndex];
+        	if (w[teamA] > 0 && l[teamB] > 0) {
+                w[teamA]--;
+                l[teamB]--;
+                startGame(gameIndex + 1);
+                w[teamA]++;
+                l[teamB]++;
+            }
+            
+            if (b[teamA] > 0 && b[teamB] > 0) {
+                b[teamA]--;
+                b[teamB]--;
+                startGame(gameIndex + 1);
+                b[teamA]++;
+                b[teamB]++;
+            }
+            if (l[teamA] > 0 && w[teamB] > 0) {
+                l[teamA]--;
+                w[teamB]--;
+                startGame(gameIndex + 1);
+                l[teamA]++;
+                w[teamB]++;
+            }
         }
     }
-
     
-
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        
+        for (int i = 0; i < 4; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            w = new int[7];
+            b = new int[7];
+            l = new int[7];
+            isCould = false;
+            
+            boolean isValid = true;
+            int totalGames = 0;
+            
+            for (int j = 1; j <= 6; j++) {
+                w[j] = Integer.parseInt(st.nextToken());
+                b[j] = Integer.parseInt(st.nextToken());
+                l[j] = Integer.parseInt(st.nextToken());
+                totalGames += w[j] + b[j] + l[j];
+                
+                if ((w[j] + b[j] + l[j]) != 5) isValid = false;
+            }
+            
+            if (isValid && totalGames == 30) {
+                startGame(0); 
+            } else {
+                isValid = false;
+            }
+            
+            System.out.print(isCould && isValid ? "1 " : "0 ");
+        }
+    }
 }
