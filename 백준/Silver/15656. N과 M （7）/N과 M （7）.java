4e3bd664e47e62209cf709932b11e92bbb57ca34
최근혁 <unlxrmsgur@naver.com>
2024-02-29 15:36:40 +0900

    [Silver III] Title: N과 M (7), Time: 788 ms, Memory: 264788 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.java
@@ -0,0 +1,41 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int N,M;
+    static int nums[];
+    static int p[];
+    static boolean isVisit[];
+    static StringBuilder sb= new StringBuilder("");
+    static void perm(int cnt) {
+    	if( cnt ==M ) {
+    		for(int i=0; i< M; i++) {
+    			sb.append(p[i]+ " ");
+    		}
+    		sb.append("\n");
+    		return;
+    	}
+    	for(int i=0; i< N; i++) {
+    		p[cnt] = nums[i];
+        	perm(cnt+1);
+    	}
+    }
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        isVisit = new boolean[N];
+        nums = new int[N];
+        p = new int[M];
+        st = new StringTokenizer(br.readLine(), " ");
+        for(int i=0; i< N; i++) {
+        	nums[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(nums);
+        perm(0);
+        System.out.println(sb.toString());
+    }
+}
