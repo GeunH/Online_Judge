a9f08a82339c294439d1194535803dae0471b46f
최근혁 <unlxrmsgur@naver.com>
2024-03-13 15:14:18 +0900

    [Gold I] Title: 비숍, Time: 140 ms, Memory: 15340 KB -BaekjoonHub

--- 백준/Gold/1799. 비숍/비숍.java
+++ 백준/Gold/1799. 비숍/비숍.java
@@ -6,9 +6,11 @@ public class Main {
 	static int [][]map;
 	static boolean []diag;
 	static boolean []reDiag;
-	static int answer;
+	static int answerW;
+	static int answerB;
 	
-	static ArrayList<Position> []diags;
+	static ArrayList<Position> []wdiags;
+	static ArrayList<Position> []bdiags;
 	
 	static class Position{
 		int y;
@@ -19,23 +21,40 @@ public class Main {
 		}
 	}
 	
-	static void nQueen(int diagNum, int queenNum) {
+	static void nQueenW(int diagNum, int queenNum) {
 		boolean isFound = false;
-		if ( answer < queenNum)answer =queenNum;
-		if ( queenNum == 2*N-2)return;
-		if( diagNum == 2*N-1) {
+		if ( answerW < queenNum)answerW =queenNum;
+		if( diagNum == N) {
 			return;
 		}
-		for(int i=0; i< diags[diagNum].size(); i++) {
-			Position now = diags[diagNum].get(i);
+		for(int i=0; i< wdiags[diagNum].size(); i++) {
+			Position now = wdiags[diagNum].get(i);
 			if( !diag[now.y+now.x] && !reDiag[ N-1 -now.y + now.x ]) {
 				isFound=  true;
 				diag[now.y+now.x] = reDiag[N-1 -now.y + now.x ]= true;
-				nQueen(diagNum+1, queenNum+1);
+				nQueenW(diagNum+1, queenNum+1);
 				diag[now.y+now.x] = reDiag[ N-1 -now.y + now.x]= false;
 			}
 		}
-		if(!isFound)nQueen(diagNum+1, queenNum);
+		if(!isFound)nQueenW(diagNum+1, queenNum);
+	}
+	
+	static void nQueenB(int diagNum, int queenNum) {
+		boolean isFound = false;
+		if ( answerB < queenNum)answerB =queenNum;
+		if( diagNum == N) {
+			return;
+		}
+		for(int i=0; i< bdiags[diagNum].size(); i++) {
+			Position now = bdiags[diagNum].get(i);
+			if( !diag[now.y+now.x] && !reDiag[ N-1 -now.y + now.x ]) {
+				isFound=  true;
+				diag[now.y+now.x] = reDiag[N-1 -now.y + now.x ]= true;
+				nQueenB(diagNum+1, queenNum+1);
+				diag[now.y+now.x] = reDiag[ N-1 -now.y + now.x]= false;
+			}
+		}
+		if(!isFound)nQueenB(diagNum+1, queenNum);
 	}
 	
 	public static void main(String[] args) throws IOException {
@@ -46,24 +65,30 @@ public class Main {
 		map = new int[N][N];
 		
 		diag = new boolean[2*N-1];
-		diags = new ArrayList[2*N-1];
+		wdiags = new ArrayList[N];
+		bdiags = new ArrayList[N];
 		reDiag = new boolean[2*N-1];
 		
-		for(int i=0; i< 2*N-1 ; i++) {
-			diags[i] = new ArrayList<>();
+		for(int i=0; i< N ; i++) {
+			wdiags[i] = new ArrayList<>();
+			bdiags[i] = new ArrayList<>();
 		}
 		
 		for(int i=0; i< N; i++) {
 			st = new StringTokenizer(br.readLine(), " " );
 			for(int j=0; j < N; j++) {
 				map[i][j] = Integer.parseInt(st.nextToken());
-				if(map[i][j] == 1) {
-					diags[i+j].add(new Position(i,j));
+				if(map[i][j] == 1 && (i+j)%2 ==0) {
+					wdiags[(i+j)/2].add(new Position(i,j));
+				}
+				if(map[i][j] == 1 && (i+j)%2 ==1) {
+					bdiags[(i+j)/2].add(new Position(i,j));
 				}
 			}
 		}
 		
-		nQueen(0,0);
-		System.out.println(answer);
+		nQueenW(0,0);
+		nQueenB(0,0);
+		System.out.println(answerW+answerB);
 	}
 }
