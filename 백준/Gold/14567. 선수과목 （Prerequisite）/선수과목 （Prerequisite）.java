9fe6e41bdd10cf5492c8440cf48d68eab2fd35d5
최근혁 <unlxrmsgur@naver.com>
2024-03-26 10:27:19 +0900

    [Gold V] Title: 선수과목 (Prerequisite), Time: 708 ms, Memory: 132268 KB -BaekjoonHub

--- 백준/Gold/14567. 선수과목 （Prerequisite）/선수과목 （Prerequisite）.java
+++ 백준/Gold/14567. 선수과목 （Prerequisite）/선수과목 （Prerequisite）.java
@@ -1,56 +1,66 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
-	static int N,M;
-	static List<Integer> []list;
-	
-	static int semester[];
-	
-	static class Subject{
-		int cnt;
-		int num;
-		
-		Subject(int num,int cnt){
-			this.cnt=cnt;
-			this.num=num;
-		}
-	}
-	
-	public static void main(String[] args) throws IOException{
+	static int N, M;
+	static List<Integer>[] tree;
+	static int[] indegree, res;
+	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine(), " " );
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
-		list = new ArrayList[N+1];
-		semester = new int[N+1];	
-		for(int i=1 ; i <= N; i++) {
-			list[i] = new ArrayList<>();
-			semester[i] = 1;
-		}
 		
-		for(int i=0; i< M; i++) {
-			st =new StringTokenizer(br.readLine(), " ");
-			int f = Integer.parseInt(st.nextToken());
-			int s = Integer.parseInt(st.nextToken());
+		tree = new ArrayList[N+1];
+		indegree = new int[N+1];
+		res = new int[N+1];
+		
+		for(int i=1;i<=N;i++) tree[i] = new ArrayList<>();
+		
+		for(int i=0;i<M;i++) {
+			st = new StringTokenizer(br.readLine());
+			int start = Integer.parseInt(st.nextToken());
+			int end = Integer.parseInt(st.nextToken());
 			
-			list[f].add(s);
+			tree[start].add(end);
+			indegree[end]++;
 		}
-		
-		
-		for(int i=1 ;i <= N; i++) {
-			for(int j=0; j< list[i].size(); j++) {
-				semester[list[i].get(j)] = Math.max(semester[list[i].get(j)],semester[i]+1);
+		Queue<Node> q = new LinkedList<>();
+		for(int i=1;i<=N;i++) {
+			if(indegree[i]==0) {
+				q.add(new Node(i, 1));
+				res[i] = 1;
 			}
 		}
 		
-
-		StringBuilder sb= new StringBuilder("");
-		for(int i=1; i<= N; i++) {
-			sb.append(semester[i]).append(" ");
+		while(!q.isEmpty()) {
+			Node p = q.poll();
+			
+			for(int c : tree[p.num]) {
+				indegree[c]--;
+				if(indegree[c]==0) {
+					res[c] = p.cnt+1;
+					q.add(new Node(c, res[c]));
+				}
+			}
 		}
-		System.out.println(sb.toString());
+		
+		for(int i=1;i<=N;i++)
+			System.out.printf("%d ", res[i]);
+		System.out.println();
 	}
-
+	
+	static class Node{
+		int num, cnt;
+		Node(int num, int cnt){
+			this.num = num;
+			this.cnt = cnt;
+		}
+	}
 }
