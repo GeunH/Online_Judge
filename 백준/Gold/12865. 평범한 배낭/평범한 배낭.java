71d1d90d67bf90c2e0d9457b62b2ae66141f08ae
최근혁 <unlxrmsgur@naver.com>
2024-03-26 11:52:28 +0900

    [Gold V] Title: 평범한 배낭, Time: 184 ms, Memory: 52500 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -1,25 +1,27 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main{
     static int N, K;
     static int[][] DP;
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        N = sc.nextInt();
-        K = sc.nextInt();
+    public static void main(String[] args) throws Exception{
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
 
         DP = new int[N+1][K+1];
 
         for(int i=1;i<=N;i++){
-            int w = sc.nextInt();
-            int v = sc.nextInt();
+        	st = new StringTokenizer(br.readLine());
+            int w = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
 
             for(int j=1;j<=K;j++){
-                DP[i][j] = j < w ?
-                        DP[i-1][j] :
+                DP[i][j] = j < w ? DP[i-1][j] :
                         v + DP[i-1][j-w] > DP[i-1][j] ?
-                                v + DP[i-1][j-w] :
-                                DP[i-1][j];
+                                v + DP[i-1][j-w] : DP[i-1][j];
             }
         }
         System.out.println(DP[N][K]);
