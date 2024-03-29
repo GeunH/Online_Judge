ef6116c32e15ab543bf250d774033438a3f4f577
최근혁 <unlxrmsgur@naver.com>
2024-03-29 13:43:01 +0900

    [Platinum V] Title: 전깃줄 - 2, Time: 1428 ms, Memory: 63504 KB -BaekjoonHub

---
+++ 백준/Platinum/2568. 전깃줄 － 2/전깃줄 － 2.java
@@ -0,0 +1,77 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N;
+    static ELine lines[];
+    static ELine[] arr;
+    static int[] idx;
+    static int[] prev; 
+    static Set<Integer> hi;
+    
+    static class ELine {
+        int src;
+        int dest;
+        ELine(int src, int dest) {
+            this.src = src;
+            this.dest = dest;
+        }
+    }
+
+    static int binarySearch(int i, int len) {
+        int left = 0;
+        int right = len - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (lines[i].dest < arr[mid].dest) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        arr[left] = lines[i];
+        if (left > 0) prev[i] = idx[left - 1]; 
+        idx[left] = i;
+        if (left == len) len++;
+        return len;
+    }
+
+    static void lis() {
+        int len = 0;
+        Arrays.fill(prev, -1); 
+        for (int i = 0; i < N; i++) {
+            len = binarySearch(i, len);
+        }
+        
+        int cur = idx[len - 1];
+        while (cur != -1) {
+        	hi.remove(lines[cur].src);
+            cur = prev[cur];
+        }
+        System.out.println(N-len);
+        for( int a : hi) {
+        	System.out.println(a);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        hi = new TreeSet<>();
+        
+        lines = new ELine[N];
+        arr = new ELine[N];
+        idx = new int[N];
+        prev = new int[N]; 
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            int src = Integer.parseInt(st.nextToken());
+            int dest = Integer.parseInt(st.nextToken());
+            lines[i] = new ELine(src, dest);
+            hi.add(src);
+        }
+        Arrays.sort(lines, (o1, o2) -> Integer.compare(o1.src, o2.src));
+        lis();
+    }
+}
